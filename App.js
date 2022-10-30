b9ee550465d180ad8ba403644e5777ab715b946b
BhagyeshPro <bhagyeshpaunikar2001@gmail.com>
2022-10-30 16:07:54 -0700

    dev

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Let's build Uber!</Text>
+      <Text>Let's build Uber! DEv</Text>
       <StatusBar style="auto" />
     </View>
   );
